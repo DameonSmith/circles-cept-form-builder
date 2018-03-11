a3cdfc688935d7f8665f82ee02d005687d2c86e2
Noel Cothren <noelcothren@gmail.com>
2018-03-11 15:55:52 -0600

    add new component

---
+++ src/components/PageWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page-wrapper">
+        <div class="tab-body">
+            
+        </div>
+        <div class="new-tab">
+
+        </div>
+    </div>
+</template>
+ 
+ 
+ <script>
+export default {};
+</script>
+ 
+
+ <style>
+
+</style>
+ 
