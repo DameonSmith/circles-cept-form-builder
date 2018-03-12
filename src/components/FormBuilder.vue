9a19819edff930b045bf061bf894191a04cfd637
Dameon Smith <dameonsmith76@gmail.com>
2018-03-12 00:49:38 -0600

    Added parent form builder page.

---
+++ src/components/FormBuilder.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-builder">
+    <page-wrapper ></page-wrapper>
+
+
+    {{ pages }}
+
+    <button v-on:click="changePage">Change Page 1</button>
+  </div>
+</template>
+
+
+<script>
+import PageWrapper from './PageWrapper';
+
+export default {
+  components: {
+    'page-wrapper': PageWrapper,
+  },
+  computed: {
+    pages() {
+      return this.$store.state.pages;
+    },
+  },
+  methods: {
+    changePage() {
+      this.$store.commit('changePage1');
+    },
+  },
+};
+</script>
+
+
+ <style>
+
+
+</style>
+
